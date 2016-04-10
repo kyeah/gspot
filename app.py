bc769f9e7b73c165f44dbff5a044c441bc3386a0
Kevin Yeh <kevinyeah@utexas.edu>
2016-04-09 21:51:29 -0500

    Organize methods, parallelize multiple playlists

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import re
 import asyncio
+import config
+import re
 import json
 import spotipy.util as util
 import sys
@@ -29,8 +30,79 @@ def get_google_library(g):
         dic[song['id']] = song
     return dic
 
+def login_google():
+    g = Mobileclient()
+    logged_in = g.login(config.auth['GOOGLE_EMAIL'], 
+                        config.auth['GOOGLE_PASSWORD'],
+                        Mobileclient.FROM_MAC_ADDRESS)
+
+    if not g.is_authenticated():
+        uprint("Invalid Google username/password")
+        sys.exit(1)
+
+    print("gettin' user playlist contents from google")
+    g.playlists = g.get_all_user_playlist_contents()
+
+    print("gettin' library")
+    g.library = get_google_library(g)
+
+    return g
+
+def login_spotify():
+    scope = 'playlist-modify-public playlist-modify-private'
+    token = util.prompt_for_user_token(config.auth['SPOTIFY_EMAIL'], scope)
+
+    if not token:
+        uprint("Invalid Spotify token")
+        sys.exit(1)
+
+    s = Spotify(auth=token)
+    s.username = config.auth['SPOTIFY_USERNAME']
+
+    playlists = s.user_playlists(s.username)['items']
+    s.playlists = {}
+
+    for sl in playlists:
+        s.playlists[sl['name']] = sl
+
+    s.playlist_names = s.playlists.keys()
+    return s
+
 @asyncio.coroutine
-def find_track_id(g, s, track, library):
+def transfer_playlist(g, s, playlist):
+
+    spotlist = {}
+    if playlist['name'] not in s.playlist_names:
+        print("Creating playlist '{}'".format(playlist['name']))
+        spotlist = s.user_playlist_create(s.username, playlist['name'])
+    else:
+        print("Updating playlist '{}'".format(playlist['name']))
+        spotlist = s.playlists[playlist['name']]
+
+    tasks = []
+    for track in playlist['tracks']:
+        if int(track['creationTimestamp']) > config.created_since:
+            future = asyncio.ensure_future(find_track_id(g, s, track))
+            tasks.append(future)
+
+    done, _ = yield from asyncio.wait(tasks)
+
+    track_id_results = [task.result() for task in done]
+    track_ids = [track_id for (ok, track_id) in track_id_results if ok]
+    not_found = [track_info for (ok, track_info) in track_id_results if not ok]
+    for nf in not_found:
+        print(nf)
+
+    spotinfo = s.user_playlist(s.username, playlist_id=spotlist['id'])
+    spottracks = [x['track']['id'] for x in spotinfo['tracks']['items']]
+    new_ids = [x for x in track_ids if x not in spottracks]
+
+    print("Adding {} new tracks!!!!!!".format(len(new_ids)))
+    for group in chunker(new_ids, 100):
+        s.user_playlist_add_tracks(s.username, spotlist['id'], group)
+
+@asyncio.coroutine
+def find_track_id(g, s, track):
     name = ""
     artist = ""
     if "name" in track:
@@ -42,8 +114,8 @@ def find_track_id(g, s, track, library):
             name = tr['title']
             artist = tr['artist']
         else:
-            name = library[track['trackId']]['title']
-            artist = library[track['trackId']]['artist']
+            name = g.library[track['trackId']]['title']
+            artist = g.library[track['trackId']]['artist']
 
     results = s.search('track:{} artist:{}'.format(name, artist))['tracks']['items']
     if len(results) > 0:
@@ -59,78 +131,22 @@ def find_track_id(g, s, track, library):
 
 def main():
 
-    # Google Music
-    gmail = input("Enter Google email address: ")
-    gpass = getpass("Enter Google password: ")
-    g = Mobileclient()
+    g = login_google()
+    s = login_spotify()
 
-    logged_in = g.login(gmail, gpass, Mobileclient.FROM_MAC_ADDRESS)
-    if not g.is_authenticated():
-        uprint("Invalid Google username/password")
-        sys.exit(1)
-
-    # Spotify
-    scope = 'playlist-modify-public playlist-modify-private'
-    spotify_email = input("Enter Spotify email: ")
-    spotify_username = input("Enter Spotify username: ")
-    spotify_token = util.prompt_for_user_token(spotify_email, scope)
-
-    if not spotify_token:
-        uprint("Invalid Spotify token")
-        sys.exit(1)
-
-    s = Spotify(auth=spotify_token)
-    created_since = 0
+    if len(sys.argv) > 1:
+        g.playlists = [p for p in g.playlists if p['name'] in sys.argv[1:]]
 
     # Transfer playlists
-    print("gettin' user playlist contents")
-    playlists = g.get_all_user_playlist_contents()
-    if len(sys.argv) > 1:
-        playlists = [p for p in playlists if p['name'] in sys.argv]
-
-    print("gettin' library")
-    library = get_google_library(g)
-
     print("time to transfer some playlistssss")
-    spotify_lists = s.user_playlists(spotify_username)['items']
-    spotify_dict = {}
-    for sl in spotify_lists:
-        spotify_dict[sl['name']] = sl
-    spotify_list_names = [x['name'] for x in spotify_lists]
+    tasks = []
+    for playlist in g.playlists:
+        future = asyncio.ensure_future(transfer_playlist(g, s, playlist))
+        tasks.append(future)
 
-    for playlist in playlists:
-        print("Transferring playlist '{}'".format(playlist['name']))
-        spotlist = {}
-        if playlist['name'] not in spotify_list_names:
-            print("Creating playlist")
-            spotlist = s.user_playlist_create(spotify_username, playlist['name'])
-        else:
-            print("Found existing playlist")
-            spotlist = spotify_dict[playlist['name']]
-
-        tasks = []
-        print("Searching for Spotify IDs")
-        for track in playlist['tracks']:
-            if int(track['creationTimestamp']) > created_since:
-                future = asyncio.ensure_future(find_track_id(g, s, track, library))
-                tasks.append(future)
-
-        loop = asyncio.get_event_loop()
-        done, _ = loop.run_until_complete(asyncio.wait(tasks))
-        loop.close()
-        track_id_results = [task.result() for task in done]
-        track_ids = [track_id for (ok, track_id) in track_id_results if ok]
-        not_found = [track_info for (ok, track_info) in track_id_results if not ok]
-        for nf in not_found:
-            print(nf)
-
-        spotinfo = s.user_playlist(spotify_username, playlist_id=spotlist['id'])
-        spottracks = [x['track']['id'] for x in spotinfo['tracks']['items']]
-        new_ids = [x for x in track_ids if x not in spottracks]
-
-        print("Adding {} new tracks!!!!!!".format(len(new_ids)))
-        for group in chunker(new_ids, 100):
-            s.user_playlist_add_tracks(spotify_username, spotlist['id'], group)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
 
 if __name__ == '__main__':
     main()
---
+++ config.py
@@ -0,0 +1,8 @@
+auth = {
+    "GOOGLE_EMAIL": "example@gmail.com",
+    "GOOGLE_PASSWORD": "totally_insecure_text_password",
+    "SPOTIFY_EMAIL": "exammple@gmail.com",
+    "SPOTIFY_USERNAME": "1262877625",
+}
+
+created_since = 0
